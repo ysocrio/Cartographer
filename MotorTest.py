539629207acc27df5d6ef8afb793c9ff386514e8
ysocrio <mickztlin@gmail.com>
2018-10-07 16:29:44 -0700

    Update MotorTest.py

--- MotorTest.py
+++ MotorTest.py
@@ -22,45 +22,61 @@ Motor2RightDir = 21
 #prompt for user input to start
 userInput = input("Press Enter To Start Test")
 
+#set all pin modes
+Mypi.set_mode(Motor1LeftEn, OUTPUT)
+Mypi.set_mode(Motor1LeftDir, OUTPUT)
+Mypi.set_mode(Motor2LeftEn, OUTPUT)
+Mypi.set_mode(Motor2LeftDir, OUTPUT)
+Mypi.set_mode(Motor1RightEn, OUTPUT)
+Mypi.set_mode(Motor1RightDir, OUTPUT)
+Mypi.set_mode(Motor2RightEn, OUTPUT)
+Mypi.set_mode(Motor2RightDir, OUTPUT)
+
 #sets range for pwm to 0-1000
-pi.set_PWM_range(Motor1LeftEn, 100)
-pi.set_PWM_range(Motor2LeftEn, 100)
-pi.set_PWM_range(Motor1RightEn, 100)
-pi.set_PWM_range(Motor2RightEn, 100)
+Mypi.set_PWM_range(Motor1LeftEn, 100)
+Mypi.set_PWM_range(Motor2LeftEn, 100)
+Mypi.set_PWM_range(Motor1RightEn, 100)
+Mypi.set_PWM_range(Motor2RightEn, 100)
 
 #set pwm frequency for the motors
-set_PWM_frequency(Motor1LeftEn,1000)
-set_PWM_frequency(Motor2LeftEn,1000)
-set_PWM_frequency(Motor1RightEn,1000)
-set_PWM_frequency(Motor2RightEn,1000)
+Mypi.set_PWM_frequency(Motor1LeftEn,1000)
+Mypi.set_PWM_frequency(Motor2LeftEn,1000)
+Mypi.set_PWM_frequency(Motor1RightEn,1000)
+Mypi.set_PWM_frequency(Motor2RightEn,1000)
+
+#set all motor directions
+Mypi.write(Motor1LeftDir,1) #one for forward, zero for reverse
+Mypi.write(Motor2LeftDir,1)
+Mypi.write(Motor1RightDir,1)
+Mypi.write(Motor2RightDir,1)
 
 #ramp up and down each motor one at a time
 for currentDuty in range(0, 100):
-  pi.set_PWM_dutycycle(Motor1LeftEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor1LeftEn,currentDuty)
   time.sleep(incrementTime)
 for currentDuty in range(100, 0):
-  pi.set_PWM_dutycycle(Motor1LeftEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor1LeftEn,currentDuty)
   time.sleep(incrementTime)
 
 for currentDuty in range(0, 100):
-  pi.set_PWM_dutycycle(Motor2LeftEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor2LeftEn,currentDuty)
   time.sleep(incrementTime)
 for currentDuty in range(100, 0):
-  pi.set_PWM_dutycycle(Motor2LeftEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor2LeftEn,currentDuty)
   time.sleep(incrementTime)
 
 for currentDuty in range(0, 100):
-  pi.set_PWM_dutycycle(Motor1RightEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor1RightEn,currentDuty)
   time.sleep(incrementTime)
 for currentDuty in range(100, 0):
-  pi.set_PWM_dutycycle(Motor1RightEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor1RightEn,currentDuty)
   time.sleep(incrementTime)
 
 for currentDuty in range(0, 100):
-  pi.set_PWM_dutycycle(Motor2RightEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor2RightEn,currentDuty)
   time.sleep(incrementTime)
 for currentDuty in range(100, 0):
-  pi.set_PWM_dutycycle(Motor2RightEn,currentDuty)
+  Mypi.set_PWM_dutycycle(Motor2RightEn,currentDuty)
   time.sleep(incrementTime)
 
 #for loop start cycles through each motor
@@ -69,6 +85,6 @@ for currentDuty in range(100, 0):
 #for loop end
 
 #release pi processes
-pi.stop()
+Mypi.stop()
 #stop the pigpio daemon
 os.system("sudo killall pigpiod")
