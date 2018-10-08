26c9a4247e5c466c0ded9ea61261578b7eb737e6
ysocrio <mickztlin@gmail.com>
2018-10-07 21:14:33 -0700

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -11,17 +11,18 @@ class Motor:
     #set PWM Freq
     gpioPi.set_PWM_frequency(motorEnable, pwmFreq)
     gpioPi.set_PWM_frequency(motorDirection, pwmFreq)
-  def speed(self, signedSpeed):
+  def Speed(self, signedSpeed):
     if signedSpeed >= 0:
       gpioPi.write(motorDirection,1)
       gpioPi.set_PWM_dutycycle(motorEnable,signedSpeed)
     ele:
       gpioPi.write(motorDirection,0)
       gpioPi.set_PWM_dutycycle(motorEnable,abs(signedSpeed))
- class TankMix:
+ class TankChasis:
   """A class that creates and controls four motors"""
   def __init__(self, pigpioObject, motorArray, pwmRange, pwmFreq):
     FrontLeftMotor = Motor(pigpioObject, motorArray[1][1], motorArray[1][2], pwmRange, pwmFreq) 
     BackLeftMotor = Motor(pigpioObject, motorArray[2][1], motorArray[2][2], pwmRange, pwmFreq) 
     FrontRightMotor = Motor(pigpioObject, motorArray[3][1], motorArray[3][2], pwmRange, pwmFreq) 
     BackRightMotor = Motor(pigpioObject, motorArray[4][1], motorArray[4][2], pwmRange, pwmFreq)
+    
