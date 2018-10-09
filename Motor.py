241a324f04e335f3fd40267bb9d5c84328d8991c
ysocrio <mickztlin@gmail.com>
2018-10-08 19:22:42 -0700

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -21,8 +21,8 @@ class Motor:
  class TankChasis:
   """A class that creates and controls four motors"""
   def __init__(self, pigpioObject, motorPinsArray, pwmRange, pwmFreq):
-    self.FrontLeftMotor = Motor(pigpioObject, motorArray[0][0], motorArray[0][1], pwmRange, pwmFreq) 
-    self.BackLeftMotor = Motor(pigpioObject, motorArray[1][0], motorArray[1][1], pwmRange, pwmFreq) 
-    self.FrontRightMotor = Motor(pigpioObject, motorArray[2][0], motorArray[2][1], pwmRange, pwmFreq) 
-    self.BackRightMotor = Motor(pigpioObject, motorArray[3][0], motorArray[3][1], pwmRange, pwmFreq)
+    self.FrontLeftMotor = Motor(pigpioObject, motorPinsArray[0][0], motorPinArray[0][1], pwmRange, pwmFreq) 
+    self.BackLeftMotor = Motor(pigpioObject, motorPinArray[1][0], motorPinArray[1][1], pwmRange, pwmFreq) 
+    self.FrontRightMotor = Motor(pigpioObject, motorPinArray[2][0], motorPinArray[2][1], pwmRange, pwmFreq) 
+    self.BackRightMotor = Motor(pigpioObject, motorPinArray[3][0], motorPinArray[3][1], pwmRange, pwmFreq)
     
