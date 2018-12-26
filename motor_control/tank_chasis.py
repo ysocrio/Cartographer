9d6eb4b5696dec3d8fad6363139ce4be8016bab4
ysocrio <mickztlin@gmail.com>
2018-12-25 20:20:38 -0800

    Update tank_chasis.py

--- motor_control/tank_chasis.py
+++ motor_control/tank_chasis.py
@@ -1,8 +1,8 @@
-import Motor from motor
+from motor import Motor
 class TankChasis:
     """A class that creates and controls four motors"""
     def __init__(self, pigpioObject, motorPinsArray, pwmRange, pwmFreq):
-        self.FrontLeftMotor = Motor(pigpioObject, motorPinsArray[0][0], motorPinArray[0][1], pwmRange, pwmFreq)
-        self.BackLeftMotor = Motor(pigpioObject, motorPinArray[1][0], motorPinArray[1][1], pwmRange, pwmFreq)
-        self.FrontRightMotor = Motor(pigpioObject, motorPinArray[2][0], motorPinArray[2][1], pwmRange, pwmFreq)
-        self.BackRightMotor = Motor(pigpioObject, motorPinArray[3][0], motorPinArray[3][1], pwmRange, pwmFreq)
+        self.FrontLeftMotor = Motor(pigpioObject, motorPinsArray[0][0], motorPinsArray[0][1], pwmRange, pwmFreq)
+        self.BackLeftMotor = Motor(pigpioObject, motorPinsArray[1][0], motorPinsArray[1][1], pwmRange, pwmFreq)
+        self.FrontRightMotor = Motor(pigpioObject, motorPinsArray[2][0], motorPinsArray[2][1], pwmRange, pwmFreq)
+        self.BackRightMotor = Motor(pigpioObject, motorPinsArray[3][0], motorPinsArray[3][1], pwmRange, pwmFreq)
