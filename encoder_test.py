893087f3013392fa9004d6b4537cca2c81c3b3bd
ysocrio <mickztlin@gmail.com>
2018-12-25 20:38:09 -0800

    Update encoder_test.py

--- encoder_test.py
+++ encoder_test.py
@@ -5,7 +5,7 @@ import motor_control.config as motor_config
 
 MyPi = pigpio.pi() #create an instance of the pi class called MyPi
 Chasis = Robot.TankChasis(MyPi, motor_config.List_Of_Pins, motor_config.PWM_Range, motor_config.PWM_Frequency)
-Chasis.FrontLeftMotor.speed(100)
-Chasis.FrontRightMotor.speed(100)
-Chasis.BackLeftMotor.speed(100)
-Chasis.BackRightMotor.speed(100)
+Chasis.FrontLeftMotor.speed(00)
+Chasis.FrontRightMotor.speed(00)
+Chasis.BackLeftMotor.speed(00)
+Chasis.BackRightMotor.speed(00)
