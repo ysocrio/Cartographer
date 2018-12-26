5b0aeefba2d49b66556c5844565dfe9194be47c9
ysocrio <mickztlin@gmail.com>
2018-12-25 20:09:14 -0800

    Updates
    -Started to write encoder_tes.py function. It will utilize the motor control package, so it will be under the root directory.
    -updated the config.py for the motors

--- motor_control/config.py
+++ motor_control/config.py
@@ -1,2 +1,9 @@
 PWM_Range = 100
 PWM_Frequency = 1000
+
+# Motor Pinouts (the following are GPIO labels)
+Front_Left_Pins = [13,5]   # [enable pin, direction pin]
+Back_Left_Pins = [19,6]   # [enable pin, direction pin]
+Front_Right_Pins = [26,8]   # [enable pin, direction pin]
+Back_Right_Pins = [25,7]   # [enable pin, direction pin]
+List_Of_Pins = [Front_Left_Pins, Back_Left_Pins, Front_Right_Pins, Back_Right_Pins]
