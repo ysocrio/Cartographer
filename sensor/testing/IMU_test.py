e99951192ce8b2456ef5ac14869a38c098b3c2f5
ysocrio <mickztlin@gmail.com>
2019-01-01 15:30:32 -0800

    Update IMU_test.py
    wrong address for ST_RESULT

--- sensor/testing/IMU_test.py
+++ sensor/testing/IMU_test.py
@@ -19,7 +19,7 @@ READ = 6
 WRITE = 7
 WRITE_MASK = 0b0
 READ_MASK = 0b1
-ST_RESULT_ADDRESS = 0x0F    # in page 0
+ST_RESULT_ADDRESS = 0x36    # in page 0
 
 i2c_bus = pi.bb_i2c_open(SDA, SCL, baud)
 
