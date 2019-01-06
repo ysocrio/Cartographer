86cad5e7de530107d8a7cebe33da13b560cbd958
ysocrio <mickztlin@gmail.com>
2019-01-05 21:52:27 -0800

    Update Cartographer.py

--- Cartographer.py
+++ Cartographer.py
@@ -3,12 +3,12 @@ if __name__ == '__main__':
     # TODO: Launch control, motor, sensor modules.
     import pigpio
     import control_module.WASD_control as control
-    import motor_control.tank_chasis as tank_chasis
+    import motor_control.tank_chasis as chasis
     import motor_control.config as motor_config
 
     MyPi = pigpio.pi() #create an instance of the pi class called MyPi
     controller = control.WASD_control()
-    robot = tank_chasis.tank_chasis(MyPi, motor_config.List_Of_Pins, motor_config.PWM_Range, motor_config.PWM_Frequency)
+    robot = tank_chasis.Tank_Chasis(MyPi, motor_config.List_Of_Pins, motor_config.PWM_Range, motor_config.PWM_Frequency)
 
     while(1):
         [left_speed, right_speed] = controller.update()
