005c81610291fbbe515f2a8795d0f1f77b727a02
ysocrio <mickztlin@gmail.com>
2019-01-05 23:24:02 -0800

    Update WASD_control.py

--- control_module/WASD_control.py
+++ control_module/WASD_control.py
@@ -17,7 +17,7 @@ class WASD_control:
         self._current_time = 0
         self._previous_time = 0
         self._previous_keypress_time = 0
-        self._keypress_timeout = .1
+        self._keypress_timeout = .15
         self._val = [0,0]
         #prompt user about changing keyboard settings
         print("change keyboard settings to have a short repeat delay")
